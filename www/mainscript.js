67e6d08eee926d4c9ae0d2e38cb293cc12e0ca88
Patrick Lucas <57973233+closeluca1@users.noreply.github.com>
2021-03-22 00:29:21 -0300

    Delete www directory

--- www/mainscript.js
+++
@@ -1,8 +0,0 @@
-function AbrirPage(){
-    let x = document.getElementById("single-page-contato");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
-    };
-};
